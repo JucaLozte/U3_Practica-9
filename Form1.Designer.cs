7059d7e58549ca8554b753d64c9ffa916f50c953
JucaLozte <carloste653@gmail.com>
2023-11-07 12:51:20 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace U3_Practica_9
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            listBox1 = new ListBox();
+            button1 = new Button();
+            label2 = new Label();
+            label3 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(30, 45);
+            label1.Name = "label1";
+            label1.Size = new Size(194, 21);
+            label1.TabIndex = 0;
+            label1.Text = "Factorial de un número";
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Arial", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox1.Location = new Point(54, 107);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 28);
+            textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Arial", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox2.Location = new Point(258, 283);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(125, 28);
+            textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Arial", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox3.Location = new Point(101, 331);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(282, 28);
+            textBox3.TabIndex = 3;
+            // 
+            // listBox1
+            // 
+            listBox1.BackColor = Color.Crimson;
+            listBox1.Font = new Font("Arial", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 21;
+            listBox1.Location = new Point(249, 35);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(150, 193);
+            listBox1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Arial", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.Location = new Point(75, 167);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 5;
+            button1.Text = "Calcular";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Arial", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(64, 286);
+            label2.Name = "label2";
+            label2.Size = new Size(188, 21);
+            label2.TabIndex = 6;
+            label2.Text = "El factorial del número";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Arial", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(66, 334);
+            label3.Name = "label3";
+            label3.Size = new Size(29, 21);
+            label3.TabIndex = 7;
+            label3.Text = "es";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.IndianRed;
+            ClientSize = new Size(460, 414);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(button1);
+            Controls.Add(listBox1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Factoriales";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private ListBox listBox1;
+        private Button button1;
+        private Label label2;
+        private Label label3;
+    }
+}
